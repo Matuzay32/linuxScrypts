51041304fe3d8be5d80bab9d57f0e24ac1fbb530
linuxlite <eze.rey92@gmail.com>
2022-06-01 15:37:09 -0300

    curso s3

--- curso3.sh
+++ curso3.sh
@@ -1,1 +1,9 @@
-#!usr/bin/bash
+#!/bin/bash
+clear
+nombre=$1
+apellido=$2
+ubicacion=$(pwd)
+echo "Hola mi nombre es :$nombre mi apellido es : $apellido"
+echo "cantidad de parametros enviados es : $#"
+echo "Los parametros enviados por el usuario son : $*"
+echo $ubicacion
